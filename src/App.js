d503b940a8d94802df0126d2c9b89d4653087e76
Abhishek <abhismart098@gmail.com>
2023-11-20 04:56:02 +0530

    the code show the run time error

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter as Router, Route,Routes}from "react-router-dom"
+import './App.css';
+import Home from "./components/Home/Home";
+import Header from "./components/Header";
+
+
+function App() {
+  return <Router>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </Router>
+    
+      
+
+}
+
+export default App;
