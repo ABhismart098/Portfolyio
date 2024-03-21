58c58ede3cc60ef1e6cfee864227f42cbb31d2a3
unknown <abhismart098@gmail.com>
2024-03-21 19:12:55 +0530

    Project section is completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./components/Home/Home";
 import Header from "./components/Hader/Header";
 import Footer from "./components/footer/footer";
 import About from "./components/About/about";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/contact";
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="/projects" element={<Projects/>} />
+      <Route path="/contact" element={<Contact />} />
 </Routes>
      <Footer />
     
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './Projects.css'; // Assuming the correct path to your CSS file
+import { Typography } from '@mui/material';
+import { AiOutlineProject } from 'react-icons/ai';
+import { Delete } from '@mui/icons-material';
+import  Image  from '../../img/pro1.png';
+import {FaRegSmileWink } from "react-icons/fa"
+ 
+
+
+const ProjectCard = ({
+  url,
+  projectImage,
+  projectTitle,
+  description,
+  technologies,
+  isAdmin = false,
+  
+}) =>{
+  return (
+    <>
+    <a href={url} className="projectCard" target="black">
+        <div>
+          <img src={projectImage} alt="Project" />
+          <Typography variant="h5">{projectTitle}</Typography>
+        </div>
+        <div>
+          <Typography variant="h4"> About Project</Typography>
+          <Typography>{description}</Typography>
+          <Typography variant="h6">Tech Stack: {technologies}</Typography>
+        </div>
+      </a>
+    {isAdmin &&(
+      <button style={{color: "rgba(40,40,40,0.7)"}}>
+        <Delete />
+      </button>
+    )}
+    </>
+    )
+}
+
+const Projects = () => {
+  const project = [1,2,3,4];
+  return (
+    <div className='projects'>
+      <Typography variant="h3">
+        Project <AiOutlineProject />
+      </Typography>
+      <div className='projectswrapper'>
+        {project.map((project, index) => (
+        <ProjectCard 
+        url = "https://abhisheky20.netlify.app/"
+  projectImage = {Image}
+  projectTitle = "Portfolio"
+  description = "This is a portfolio useing basic Html css and javascript library for it is provide all basic functionality"
+  technologies = "Html,css,js," 
+  />
+        ))}
+      </div>
+      <Typography variant = "h3" style={{ font: "100 1.2rem 'Ubuntu Mono'"}} >
+      All the projects here are prepared by abhisheky20 <FaRegSmileWink />
+
+      </Typography>
+    </div>
+  );
+}
+
+export default Projects;
